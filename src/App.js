e00b708a5b17496fe8061bee1a44912127b6ed1a
chelpa <fcopavicevic@gmail.com>
2024-04-05 15:09:17 -0300

    changes for v3

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from <code>chazeware/AppFinance.js</code> 
+          Hello from react
         </p>
-        <a
-          className="App-link"
-          href="https://wwww.seamospartners.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
 
-        </a>
       </header>
     </div>
   );
